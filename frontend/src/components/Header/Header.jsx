a2ae05ba9ac26154ea50b608be70f922753b93e8
SumethVindunu <it225923932@my.sliit.lk>
2024-07-29 19:54:57 +0530

    day 1

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Our food delivery app makes it easy to order your favorite meals from a variety of restaurants. Enjoy delicious food delivered to your doorstep, whether it's a hearty dinner, light lunch, or sweet treat. Quick and reliable service with just a few taps on your phone.</p>
+            <button>View Menu</button>
+        </div>
+      </div>
+  )
+}
+
+export default Header
